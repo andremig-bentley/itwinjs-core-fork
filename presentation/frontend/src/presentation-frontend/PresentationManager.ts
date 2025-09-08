0cc0c12b65cc76a278d401fb5e3c87a1314a78cc
andremig-bentley <andremig-bentley@users.noreply.github.com>
2025-09-07 23:15:32 -0600

    non dep change

--- presentation/frontend/src/presentation-frontend/PresentationManager.ts
+++ presentation/frontend/src/presentation-frontend/PresentationManager.ts
@@ -563,6 +563,7 @@ export class PresentationManager implements Disposable {
     }
   }
   //adding an unnecessary comment
+  //anothero ne
 
   /** Retrieves overall content set size.
    * @deprecated in 4.5 - will not be removed until after 2026-09-05. Use something else instead.
